823da560087ef0a189a6a39a75425c4bef73ccfa
Leo Nguyen <leo.nguyen.goldenowl@gmail.com>
2020-12-23 11:53:25 +0700

    test selenium on heroku

--- src/controllers/tickets.js
+++ src/controllers/tickets.js
@@ -1,3 +1,4 @@
+const { until, By } = require('selenium-webdriver')
 const { driver } = require('../constants')
 const { DriverHelper, generateResponse } = require('../helpers')
 const { loginWithDefaultAccount } = require('./auth')
@@ -14,14 +15,24 @@ module.exports = new (class TicketController {
     })
     try {
       await loginWithDefaultAccount(webDriver)
-
+      const elementCalendar = await webDriver.wait(
+        until.elementLocated(By.className('ant-calendar-picker-input'))
+      )
+      // .click()
+      // await webDriver
+      //   .findElement(By.xpath("//td[@title='December 25, 2020']"))
+      //   .click()
       const response = generateResponse({
-        success   : true,
-        statusCode: 200,
-        message   : 'Login Successfully!!!'
+        statusSuccess: true,
+        statusCode   : 200,
+        message      : 'Login Successfully!!!',
+        result       : {
+          elementCalendar
+        }
       })
       return res.json(response)
     } catch (error) {
+      console.log(error)
       res.status(500).json({ msg: 'Server error...' })
     }
   }
